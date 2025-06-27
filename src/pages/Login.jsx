a5f4edaa2d41612f5ac9297fa5386806978eda28
tratchapong <tratchapong@gmail.com>
2025-06-27 10:30:09 +0700

    Register with api : done + toastify

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,17 +9,19 @@ import Register from './Register'
 
 function Login() {
 	const [resetForm, setResetForm] = useState(false)
-	const {handleSubmit, register, formState: {errors, isSubmitting}, reset} = useForm({
-		resolver : yupResolver(loginSchema),
+	const { handleSubmit, register, formState: { errors, isSubmitting }, reset } = useForm({
+		resolver: yupResolver(loginSchema),
 	})
- 	const hdlClose = () => {``
+	const hdlClose = () => {
+		``
 		console.log('dialog close...')
-		setResetForm(prv=>!prv)
+		setResetForm(prv => !prv)
 	}
-	const hdlLogin = data => {
-		alert(JSON.stringify(data,null,2))
+	const hdlLogin = async data => {
+		await new Promise(resolve => setTimeout(resolve, 2000))
+		alert(JSON.stringify(data, null, 2))
 	}
-	
+
 	return (
 		<>
 			<div className="h-[700px] pt-20 pb-28">
@@ -33,26 +35,31 @@ function Login() {
 					<div className='flex flex-1 bg-white'>
 						<div className="card bg-base-100 w-full h-[350px] shadow-xl mt-8">
 							<form onSubmit={handleSubmit(hdlLogin)}>
-								<div className="card-body">
-									<input type='text'
-										className='input w-full'
-										placeholder='E-mail or Phone number'
-										{...register('identity')}
-									/>
-									{errors.identity?.message && <p className='text-sm text-error'>{errors.identity.message}</p>}
-									<input type='password'
-										className='input w-full'
-										placeholder='password'
-										{...register('password')}
+								<fieldset disabled={isSubmitting}>
+									<div className="card-body">
+										<input type='text'
+											className='input w-full'
+											placeholder='E-mail or Phone number'
+											{...register('identity')}
 										/>
-									{errors.password?.message && <p className='text-sm text-error'>{errors.password.message}</p>}
-									<button className='btn btn-primary text-xl'>Login</button>
-									<p className="text-center cursor-pointer opacity-70">Forgotten password</p>
-									<div className="divider my-0"></div>
-									<button type="button" className='btn btn-secondary text-lg'
-										onClick={()=>document.getElementById('register-form').showModal()}
-									>Create new account</button>
-								</div>
+										{errors.identity?.message && <p className='text-sm text-error'>{errors.identity.message}</p>}
+										<input type='password'
+											className='input w-full'
+											placeholder='password'
+											{...register('password')}
+										/>
+										{errors.password?.message && <p className='text-sm text-error'>{errors.password.message}</p>}
+										{!isSubmitting && <button className='btn btn-primary text-xl'>Login</button>}
+										{isSubmitting && <button className='btn btn-primary text-xl'>Login
+											<span className="loading loading-bars loading-md"></span>
+										</button>}
+										<p className="text-center cursor-pointer opacity-70">Forgotten password</p>
+										<div className="divider my-0"></div>
+										<button type="button" className='btn btn-secondary text-lg'
+											onClick={() => document.getElementById('register-form').showModal()}
+										>Create new account</button>
+									</div>
+								</fieldset>
 							</form>
 						</div>
 					</div>
@@ -60,7 +67,7 @@ function Login() {
 			</div>
 			<dialog id="register-form" className="modal" onClose={hdlClose}>
 				<div className="modal-box">
-					<Register resetForm={resetForm}/>
+					<Register resetForm={resetForm} />
 					<form method="dialog">
 						<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
 					</form>
