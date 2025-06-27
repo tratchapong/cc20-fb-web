435619d379732481b217b9a2318eacbeb7ec0709
tratchapong <tratchapong@gmail.com>
2025-06-27 14:46:47 +0700

    Header start ..

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,50 @@
+import { Link } from 'react-router'
+import { FakebookLogo, FriendsIcon, HomeIcon, MenuIcon, MessengerIcon, NotificationIcon, PlayIcon, SearchIcon, ShopIcon } from '../icons'
 import useUserStore from '../stores/userStore'
 
 function Header() {
 	const logout = useUserStore(state => state.logout)
-	
+
 	return (
-		<div className='text-4xl py-4 border'>Header
-			<button className='btn btn-primary' onClick={logout}>Logout</button>
+		<div className='flex justify-between px-3 h-14 w-full shadow-lg bg-white fixed top-0 z-10'>
+			{/* Search Bar */}
+			<div className="flex-1 flex gap-2 items-center pe-2">
+				<FakebookLogo className='w-12' />
+				<label className="input rounded-full">
+					<input  placeholder="Search" />
+					<SearchIcon className="w-5 opacity-60" />
+				</label>
+			</div>
+			{/* Navigator */}
+			<div className='flex-1 flex gap-2 justify-center'>
+				<Link to='/' className='flex justify-center w-20 hover:outline-2 hover:outline-blue-900'
+				> <HomeIcon className='w-1/2' /> </Link>
+				<Link to='/' className='flex justify-center w-20 hover:outline-2 hover:outline-blue-900'
+				> <PlayIcon className='w-1/2' /> </Link>
+				<Link to='/' className='flex justify-center w-20 hover:outline-2 hover:outline-blue-900'
+				> <ShopIcon className='w-3/5' /> </Link>
+				<Link to='/friends' className='flex justify-center w-20 hover:outline-2 hover:outline-blue-900'
+				> <FriendsIcon className='w-1/2' /> </Link>
+			</div>
+
+			{/* Right + Dropdown Menu */}
+			<div className='flex-1 flex gap-3 justify-end'>
+				<div className="avatar justify-center items-center">
+					<div className="w-10 h-10 rounded-full !flex justify-center items-center bg-gray-300 hover:bg-gray-400">
+						<MenuIcon className='w-5' />
+					</div>
+				</div>
+				<div className="avatar justify-center items-center">
+					<div className="w-10 h-10 rounded-full !flex justify-center items-center bg-gray-300 hover:bg-gray-400">
+						<MessengerIcon className='w-5' />
+					</div>
+				</div>
+				<div className="avatar justify-center items-center">
+					<div className="w-10 h-10 rounded-full !flex justify-center items-center bg-gray-300 hover:bg-gray-400">
+						<NotificationIcon className='w-5' />
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
