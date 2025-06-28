0ecb4847b47b062ca3dcdaf8cba078a2c871edcd
tratchapong <tratchapong@gmail.com>
2025-06-28 16:52:09 +0700

    SidebarContact + prepare PostContainer

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function PostContainer() {
   return (
     <div className='w-[680px] mx-auto min-h-screen my-3 flex flex-col gap-4 rounded-lg bg-amber-200'>
-      PostContainer
+      <div>CreatePost</div>
+      <div>PostItem</div>
+      <div>PostItem</div>
+      <div>PostItem</div>
     </div>
   )
 }
