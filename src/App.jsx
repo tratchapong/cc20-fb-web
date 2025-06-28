f03645a413e1282389dfb773fda9efb44ce77c71
tratchapong <tratchapong@gmail.com>
2025-06-28 15:08:37 +0700

    Insert : check Token expire when app start

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import AppRouter from "./routes/AppRouter"
+import useUserStore from './stores/userStore'
+import { useEffect } from 'react'
+import isTokenExpired from './utils/isTokenExpired'
 
 function App() {
+  const token = useUserStore(state => state.token)
+  const logout = useUserStore(state=> state.logout)
+
+  useEffect( ()=>{
+    if(isTokenExpired(token)) {
+      console.log('Token Expired...')
+      logout()
+    }
+  },[] )
 
   return (
     <>
