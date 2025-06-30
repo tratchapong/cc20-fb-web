f4c4d2138c7a1cd12d545193d70319526bd9f490
tratchapong <tratchapong@gmail.com>
2025-06-30 10:41:45 +0700

    CreatePost UI

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import useUserStore from '../stores/userStore'
 import Avatar from './Avatar'
+import { ActivityIcon, PhotoIcon, VideoIcon2 } from '../icons'
 
 function CreatePost() {
 	const user = useUserStore(state => state.user)
@@ -8,14 +9,28 @@ function CreatePost() {
 		<div className='card bg-base-100 shadow-xl'>
 			<div className='card-body p-6'>
 				<div className="flex gap-2">
-					<Avatar className='w-11 rounded-full' 
+					<Avatar className='w-11 rounded-full'
 						imgSrc={user.profileImage}
 					/>
 					<button className='btn flex-1 rounded-full justify-start'>
 						what do you think?
 					</button>
 				</div>
-				
+				<div className="divider mt-1 mb-0"></div>
+				<div className="flex gap-3">
+					<div className="flex-1 flex gap-3 justify-center cursor-pointer hover:bg-gray-300 rounded-lg py-2">
+						<VideoIcon2 className='w-6' />
+						Live / Stream
+					</div>
+					<div className="flex-1 flex gap-3 justify-center cursor-pointer hover:bg-gray-300 rounded-lg py-2">
+						<PhotoIcon className='w-6' />
+						Live / Stream
+					</div>
+					<div className="flex-1 flex gap-3 justify-center cursor-pointer hover:bg-gray-300 rounded-lg py-2">
+						<ActivityIcon className='w-6' />
+						Live / Stream
+					</div>
+				</div>
 			</div>
 		</div>
 	)
